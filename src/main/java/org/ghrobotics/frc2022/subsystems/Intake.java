6f8c5dbb0c33008340a6c99b6726776d3beb4af1
Prateek Machiraju <prateek.machiraju@gmail.com>
2022-03-01 09:48:38 -0500

    Clean up code

--- src/main/java/org/ghrobotics/frc2022/subsystems/Intake.java
+++ src/main/java/org/ghrobotics/frc2022/subsystems/Intake.java
@@ -1,37 +1,84 @@
 package org.ghrobotics.frc2022.subsystems;
 
-import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static com.revrobotics.CANSparkMax.IdleMode;
+import static com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class Intake extends SubsystemBase{
-    //Motor controllers
-    private final CANSparkMax leader;
+public class Intake extends SubsystemBase {
+  // Motor Controllers
+  private final CANSparkMax leader_;
 
-    //Pneumatics
-    private final Solenoid pivot;
+  // Pneumatics
+  private final Solenoid pivot_;
 
-    //io
-    private final PeriodicIO io_ = new PeriodicIO();
+  // IO
+  private final PeriodicIO io_ = new PeriodicIO();
 
-    //Initialize
-    leader = new CANSparkMax(Constants.kIntakeId, MotorType.kBrushless);
-    pivot = new Solenoid(PneumaticsModuleType.REVPH, Constants.kPivotId);
+  /**
+   * Constructs an instance of the Intake subsystem. Only one instance of this subsystem should
+   * be created in the main Robot class and references to this instance should be passed around
+   * the robot code.
+   */
+  public Intake() {
+    // Initialize motor controller.
+    leader_ = new CANSparkMax(Constants.kLeaderId, MotorType.kBrushless);
+    leader_.restoreFactoryDefaults();
+    leader_.setIdleMode(IdleMode.kBrake);
+    leader_.enableVoltageCompensation(12);
+    leader_.setInverted(false);
 
-    public void periodic() {
-        leader.set(io_.demand);
+    // Initialize pneumatics.
+    pivot_ = new Solenoid(PneumaticsModuleType.REVPH, Constants.kPivotId);
+  }
+
+  /**
+   * This method runs periodically every 20 ms. Here, all sensor values are read and all motor
+   * outputs should be set.
+   */
+  public void periodic() {
+    // Write outputs.
+    if (io_.wants_pneumatics_update) {
+      io_.wants_pneumatics_update = false;
+      pivot_.set(io_.pivot_value);
     }
 
-    public void setPercent(double value){
-       io_.demand = value;
-    }
+    leader_.set(io_.demand);
+  }
 
-    public static class PeriodicIO{
-        double demand;
-    }
+  /**
+   * Sets the % output on the intake.
+   *
+   * @param value The % output in [-1, 1].
+   */
+  public void setPercent(double value) {
+    io_.demand = value;
+  }
 
-    //Constants
+  /**
+   * Sets the intake pivot.
+   *
+   * @param value The pivot value; true if the intake should be extended.
+   */
+  public void setPivot(boolean value) {
+    io_.wants_pneumatics_update = value != io_.pivot_value;
+    io_.pivot_value = value;
+  }
+
+  public static class PeriodicIO {
+    // Outputs
+    double demand;
+    boolean pivot_value;
+    boolean wants_pneumatics_update;
+  }
+
+  public static class Constants {
+    // Motor Controllers
+    public static final int kLeaderId = 9;
+
+    // Pneumatics
     public static final int kPivotId = 0;
-    public static final int kIntakeId = 0;
-
+  }
 }
