a63d596b2308c8fa8cfe5b0ac5ceae995aae1786
Prateek Machiraju <prateek.machiraju@gmail.com>
2022-03-06 10:44:11 -0500

    Change auto climb to reflect new climber design

--- src/main/java/org/ghrobotics/frc2022/commands/ClimbAutomatic.java
+++ src/main/java/org/ghrobotics/frc2022/commands/ClimbAutomatic.java
@@ -19,31 +19,31 @@ public class ClimbAutomatic extends SequentialCommandGroup {
     // Create the automatic climb routine.
     addCommands(
         // Part 1: move the arms to be ready for mid rung climb.
-        new InstantCommand(() -> climber.setPivot(false, false)),
-        new ClimbToPosition(climber, 0, Constants.kReadyForL2Height),
+        new InstantCommand(() -> climber.setPivot(true, false)),
+        new ClimbToPosition(climber, Constants.kMaxHeight, Constants.kReadyForL2Height)
+            .withInterrupt(() ->
+                Math.abs(climber.getRightPosition() - Constants.kReadyForL2Height) <
+                    Climber.Constants.kErrorTolerance),
+
         new WaitUntilCommand(advance_button),
 
-        // Part 2: climb to mid rung: pull the right arm all the way down. When 2 inches from
-        // bottom, pivot right arm and extend left arm to max height. Once the left arm reaches
-        // max height, pivot it.
-        new ClimbToPosition(climber, 0, Constants.kSafeL2PivotHeight),
-        new InstantCommand(() -> climber.setPivot(false, true)),
+        // Part 2: climb to mid rung: pull the right arm all the way down. When the left arm is
+        // all the way up, un-pivot it.
         new ClimbToPosition(climber, Constants.kMaxHeight, Constants.kClimbHeight),
-        new InstantCommand(() -> climber.setPivot(true, true)),
+        new InstantCommand(() -> climber.setPivot(false, false)),
         new WaitUntilCommand(advance_button),
 
-        // Part 3: climb to high rung: pull the left all arm the way down. When 4 inches from the
-        // top, unpivot right arm and extend to max height. Once the right arm reaches max height,
-        // pivot it.
-        new ClimbToPosition(climber, Constants.kSafeL3PivotHeight, 0),
-        new InstantCommand(() -> climber.setPivot(true, false)),
+        // Part 3: climb to high rung: pull the left all arm the way down. When 8 inches from the
+        // top, pivot right arm and extend to max height. Once the right arm reaches max height,
+        // un-pivot it.
+        new ClimbToPosition(climber, Constants.kSafeL3PivotHeight, Constants.kClimbHeight),
+        new InstantCommand(() -> climber.setPivot(false, true)),
         new ClimbToPosition(climber, Constants.kClimbHeight, Constants.kMaxHeight),
-        new InstantCommand(() -> climber.setPivot(true, true)),
+        new InstantCommand(() -> climber.setPivot(false, false)),
         new WaitUntilCommand(advance_button),
 
-        // Part 4: climb to traversal rung: pull the right arm all the way down, then unpivot it.
+        // Part 4: climb to traversal rung: pull the right arm all the way down.
         new ClimbToPosition(climber, 0, Constants.kClimbHeight),
-        new InstantCommand(() -> climber.setPivot(true, false)),
         new InstantCommand(() -> climber.setBrake(true)),
 
         // Celebrate.
@@ -58,10 +58,9 @@ public class ClimbAutomatic extends SequentialCommandGroup {
   public static class Constants {
     // Heights
     public static final double kMaxHeight = Climber.Constants.kMaxHeight;
-    public static final double kClimbHeight = Units.inchesToMeters(0.0);
-    public static final double kReadyForL2Height = Units.inchesToMeters(7.0);
-    public static final double kSafeL2PivotHeight = Units.inchesToMeters(2.0);
-    public static final double kSafeL3PivotHeight = kMaxHeight - Units.inchesToMeters(4.0);
+    public static final double kClimbHeight = Units.inchesToMeters(0);
+    public static final double kReadyForL2Height = Units.inchesToMeters(17);
+    public static final double kSafeL3PivotHeight = kMaxHeight - Units.inchesToMeters(7);
 
     // Celebration
     public static final double kOrchestraWaitTime = 15;
