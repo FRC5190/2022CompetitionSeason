465933180f89b5b0fb692fc9d8306c5bdb5b6d81
Prateek Machiraju <prateek.machiraju@gmail.com>
2022-03-22 13:01:34 -0400

    Start work on new auto routines

---
+++ src/main/java/org/ghrobotics/frc2022/auto/HighRight5.java
@@ -0,0 +1,12 @@
+package org.ghrobotics.frc2022.auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ * This auto mode runs the following routine:
+ *  - 1 cargo must be preloaded
+ *  - Score 1
+ *
+ */
+public class HighRight5 extends SequentialCommandGroup {
+}
