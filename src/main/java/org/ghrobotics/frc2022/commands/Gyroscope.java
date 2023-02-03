73da67a2c5552ed5622c666129513fd42f2d1f0f
saeshaagarwal <110415532+saeshaagarwal@users.noreply.github.com>
2023-02-02 19:04:35 -0500

    Update Gyroscope.java

--- src/main/java/org/ghrobotics/frc2022/commands/Gyroscope.java
+++ src/main/java/org/ghrobotics/frc2022/commands/Gyroscope.java
@@ -8,16 +8,19 @@ import org.ghrobotics.frc2022.RobotState;
 
 public class Gyroscope extends CommandBase {
     private double motorOutput3;
+    private double [] ypr = new double[3];
     private final RobotState robotstate = new RobotState();
     Drivetrain drivetrain = new Drivetrain(robotstate);
     WPI_PigeonIMU pigeon = new WPI_PigeonIMU(0);
-
     public boolean onRamp = false;
+
+    public Gyroscope () {
+
+    }
 
     @Override
     public void execute(){
 
-        double[] ypr = new double[3];
         pigeon.getYawPitchRoll(ypr);
 
         double rollValue = ypr[2] + 0.175;
@@ -33,13 +36,15 @@ public class Gyroscope extends CommandBase {
 
                 if (rollValue <0){
                     rollValue = rollValue * -1;
-                    motorOutput3 = (Math.pow(1.03, rollValue)-1)*100;
+                    motorOutput3 = (Math.pow(1.01, rollValue)-1)*100;
                     System.out.println("Motor Output should be: " + (-1 * motorOutput3) + "%");
                 } else {
-                    motorOutput3 = (Math.pow(1.03, rollValue)-1)*100;
+                    motorOutput3 = (Math.pow(1.01, rollValue)-1)*100;
                     System.out.println("Motor Output shoud be: " + motorOutput3 + "%");
                 }
                 drivetrain.setPercent (motorOutput3, motorOutput3);
+            } else {
+                drivetrain.setPercent (0, 0);
             }
         }
     addRequirements (drivetrain);
